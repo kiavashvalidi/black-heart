324a2afcd0b02e0928386683aa98f127b4673568
kiavashvalidi <95073003+kiavashvalidi@users.noreply.github.com>
2022-10-04 17:32:07 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from turtle import *
+from time import sleep
+
+color("black")
+begin_fill()
+pensize(4)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
+sleep(5)
